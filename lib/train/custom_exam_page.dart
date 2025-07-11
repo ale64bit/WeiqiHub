c28f61ecd797c94276d806dca1bc2980eb35c897
Alejandro López <ale64bit@gmail.com>
2025-07-11 15:20:57 +0300

    Move TaskSource creation outside build

--- lib/train/custom_exam_page.dart
+++ lib/train/custom_exam_page.dart
@@ -1,10 +1,13 @@
 import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 import 'package:flutter/material.dart';
+import 'package:wqhub/settings/shared_preferences_inherited_widget.dart';
 import 'package:wqhub/stats/stats_db.dart';
 import 'package:wqhub/train/exam_page.dart';
 import 'package:wqhub/train/rank_range.dart';
 import 'package:wqhub/train/task_repository.dart';
+import 'package:wqhub/train/task_source/black_to_play_source.dart';
 import 'package:wqhub/train/task_source/const_task_ref_source.dart';
+import 'package:wqhub/train/task_source/task_source.dart';
 import 'package:wqhub/train/task_source/task_source_type.dart';
 import 'package:wqhub/train/task_tag.dart';
 import 'package:wqhub/train/task_type.dart';
@@ -32,20 +35,12 @@ class CustomExamPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final taskSource = switch (taskSourceType) {
-      TaskSourceType.fromTaskTypes =>
-        TaskRepository().taskSourceByTypes(rankRange, taskTypes!),
-      TaskSourceType.fromTaskTag =>
-        TaskRepository().taskSourceByTag(rankRange, taskTag!),
-      TaskSourceType.fromMistakes => ConstTaskRefSource(
-          taskRefs: StatsDB().getMistakesByRange(rankRange, taskCount)),
-    };
     return ExamPage(
       title: 'Custom Exam',
       taskCount: taskCount,
-      taskSource: taskSource,
       timePerTask: timePerTask,
       maxMistakes: maxMistakes,
+      createTaskSource: createTaskSource,
       onPass: () {},
       onFail: () {},
       buildRedoPage: () => CustomExamPage(
@@ -57,6 +52,21 @@ class CustomExamPage extends StatelessWidget {
         collectStats: collectStats,
       ),
       collectStats: collectStats,
+    );
+  }
+
+  TaskSource createTaskSource(BuildContext context) {
+    final taskSource = switch (taskSourceType) {
+      TaskSourceType.fromTaskTypes =>
+        TaskRepository().taskSourceByTypes(rankRange, taskTypes!),
+      TaskSourceType.fromTaskTag =>
+        TaskRepository().taskSourceByTag(rankRange, taskTag!),
+      TaskSourceType.fromMistakes => ConstTaskRefSource(
+          taskRefs: StatsDB().getMistakesByRange(rankRange, taskCount)),
+    };
+    return BlackToPlaySource(
+      source: taskSource,
+      blackToPlay: context.settings.alwaysBlackToPlay,
     );
   }
 }
