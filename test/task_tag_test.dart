9511619784044bde962e8ad002a68eeb1a69dc53
Alejandro López <ale64bit@gmail.com>
2025-10-21 00:25:08 +0300

    Fix some tag ranges

--- test/task_tag_test.dart
+++ test/task_tag_test.dart
@@ -4,9 +4,15 @@ import 'package:wqhub/train/task_tag.dart';
 void main() {
   test('tag rank range consistency', () {
     for (final tag in TaskTag.values) {
-      for (final rankRange in tag.ranks()) {
+      final ranks = tag.ranks();
+      for (final rankRange in ranks) {
         expect(rankRange.from.index <= rankRange.to.index, true,
             reason: 'tag $tag: bad range $rankRange');
+      }
+      for (int i = 0; i + 1 < ranks.length; ++i) {
+        expect(ranks[i].to.index < ranks[i + 1].from.index, true,
+            reason:
+                'tag: $tag: bad range transition ${ranks[i]} -> ${ranks[i + 1]}');
       }
     }
   });
