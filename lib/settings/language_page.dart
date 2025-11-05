d1d009f60be5650e1b101e42cfc1e06cf7e9654d
hemme <mariotti.marco@gmail.com>
2025-11-05 09:01:17 +0100

    Add Italian l10n (no taskTags, no themes) (#111)

--- lib/settings/language_page.dart
+++ lib/settings/language_page.dart
@@ -17,6 +17,7 @@ class _LanguagePageState extends State<LanguagePage> {
   static final Map<String, String> nativeLanguageName = {
     'en': 'English',
     'es': 'Español',
+    'it': 'Italiano',
     'ro': 'Română',
     'ru': 'Русский',
     'zh': '中文 – 简体',
