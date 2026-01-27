e21bcddd3f9bf0e1ee92fd5a80078071eb207f22
Alejandro López <ale64bit@gmail.com>
2026-01-27 21:09:57 +0300

    Support alignment for player cards

--- lib/play/player_card.dart
+++ lib/play/player_card.dart
@@ -1,3 +1,4 @@
+import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 import 'package:flutter/material.dart';
 import 'package:wqhub/blinking_icon.dart';
 import 'package:wqhub/game_client/user_info.dart';
@@ -6,6 +7,11 @@ import 'package:wqhub/settings/shared_preferences_inherited_widget.dart';
 import 'package:wqhub/turn_icon.dart';
 import 'package:wqhub/wq/wq.dart' as wq;
 
+enum PlayerCardAlignment {
+  left,
+  right,
+}
+
 class PlayerCard extends StatelessWidget {
   final UserInfo userInfo;
   final wq.Color color;
@@ -13,6 +19,7 @@ class PlayerCard extends StatelessWidget {
   final Widget timeDisplay;
   final Function()? onTap;
   final bool showOnlineStatus;
+  final PlayerCardAlignment alignment;
 
   const PlayerCard({
     super.key,
@@ -22,50 +29,59 @@ class PlayerCard extends StatelessWidget {
     required this.timeDisplay,
     this.onTap,
     this.showOnlineStatus = false,
+    this.alignment = PlayerCardAlignment.left,
   });
 
   @override
   Widget build(BuildContext context) {
     final loc = AppLocalizations.of(context)!;
+    final crossAxisAlignment = switch (alignment) {
+      PlayerCardAlignment.left => CrossAxisAlignment.start,
+      PlayerCardAlignment.right => CrossAxisAlignment.end,
+    };
+    final children = <Widget>[
+      SizedBox(width: 4),
+      TurnIcon(color: color),
+      SizedBox(width: 4),
+      Expanded(
+        child: Column(
+          crossAxisAlignment: crossAxisAlignment,
+          children: [
+            Text(
+              context.settings.hidePlayerRanks
+                  ? userInfo.username
+                  : '${userInfo.username} [${userInfo.rank}]',
+              overflow: TextOverflow.fade,
+              softWrap: false,
+            ),
+            Text('${loc.captures}: $captureCount',
+                style: TextTheme.of(context).labelSmall),
+          ],
+        ),
+      ),
+      if (showOnlineStatus && userInfo.online)
+        const Icon(Icons.wifi, color: Colors.green),
+      if (showOnlineStatus && !userInfo.online)
+        const BlinkingIcon(
+          icon: Icons.wifi_off,
+          color: Colors.red,
+          duration: Duration(seconds: 1),
+          curve: Curves.linear,
+        ),
+      Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: timeDisplay,
+      ),
+    ];
     return Card(
       clipBehavior: Clip.hardEdge,
       child: InkWell(
         onTap: onTap,
         child: Row(
-          children: <Widget>[
-            SizedBox(width: 4),
-            TurnIcon(color: color),
-            SizedBox(width: 4),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    context.settings.hidePlayerRanks
-                        ? userInfo.username
-                        : '${userInfo.username} [${userInfo.rank}]',
-                    overflow: TextOverflow.fade,
-                    softWrap: false,
-                  ),
-                  Text('${loc.captures}: $captureCount',
-                      style: TextTheme.of(context).labelSmall),
-                ],
-              ),
-            ),
-            if (showOnlineStatus && userInfo.online)
-              const Icon(Icons.wifi, color: Colors.green),
-            if (showOnlineStatus && !userInfo.online)
-              const BlinkingIcon(
-                icon: Icons.wifi_off,
-                color: Colors.red,
-                duration: Duration(seconds: 1),
-                curve: Curves.linear,
-              ),
-            Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: timeDisplay,
-            ),
-          ],
+          children: switch (alignment) {
+            PlayerCardAlignment.left => children,
+            PlayerCardAlignment.right => children.reversedView,
+          },
         ),
       ),
     );
